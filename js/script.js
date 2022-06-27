cc2e63237214dec64cc17f6d00cd7f1e4e63daac
pedromchd <pedromchd@outlook.com.br>
2022-06-27 16:25:26 -0300

    id nos elementos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+const elemento = document.querySelector('#12');
+
 document.addEventListener('keydown', evt => {
+  let number = evt.target.id;
   if (evt.key === 'Enter') {
-    console.log(evt.target);
+    console.log(number);
   }
 });
