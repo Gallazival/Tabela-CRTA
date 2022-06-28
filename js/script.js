554598728be5c89b593dbb567cc7355248f120ae
pedromchd <pedromchd@outlook.com.br>
2022-06-27 23:46:58 -0300

    controle pelas setas esquerda e direita

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
-const elemento = document.querySelector('#12');
-
 document.addEventListener('keydown', evt => {
-  let number = evt.target.id;
+  // console.log(evt);
   if (evt.key === 'Enter') {
-    console.log(number);
+    evt.preventDefault();
+    if (evt.target.id === 'E57-71') {
+      console.log('aaa');
+      document.querySelector('#E57').focus();
+    }
+    if (evt.target.id === 'E89-103') {
+      document.querySelector('#E89').focus();
+    }
+  }
+  if (evt.key === 'ArrowRight') {
+    evt.preventDefault();
+    if (evt.target.nextElementSibling.classList.contains('col-hidden')) {
+      evt.target.nextElementSibling.nextElementSibling.focus();
+    } else {
+      evt.target.nextElementSibling.focus();
+    }
+  }
+  if (evt.key === 'ArrowLeft') {
+    evt.preventDefault();
+    if (evt.target.previousElementSibling.classList.contains('col-hidden')) {
+      evt.target.previousElementSibling.previousElementSibling.focus();
+    } else {
+      evt.target.previousElementSibling.focus();
+    }
+  }
+  if (evt.key === 'ArrowUp') {
+    evt.preventDefault();
+    // document.querySelector(`#E${parseInt(evt.target.id.split('').shift) - 12}`).focus()
   }
 });
