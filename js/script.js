e76596adf09e118f04cdb494168aef1b96f7ad07
pedromchd <pedromchd@outlook.com.br>
2022-07-15 18:44:03 -0300

    trap focus e popup edit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import { elementos } from './elementos.js';
 // const makeEmptyArray = (size, content) => new Array(size).fill(content);
 
 const elements = document.getElementsByClassName('elemento');
+let lastFocus = document.querySelector('[data-id="1"]');
 
 for (let elemento of elements) {
   elemento.addEventListener('click', () => {
@@ -13,8 +14,33 @@ for (let elemento of elements) {
       detalharElemento(elemento.dataset.id);
     }
   });
+  elemento.addEventListener('focusout', () => {
+    return lastFocus = elemento;
+  });
 }
 
 function detalharElemento(index) {
-  console.log(elementos[index].curiosidade);
+  document.querySelector('.focus-wall').style.visibility = 'visible';
+  document.querySelector('.pop-up-text').focus();
+  document.querySelector('.pop-up-text').innerHTML = `
+    <h2>Elemento: ${elementos[index].nome}</h2>
+    <p>Curiosidade: ${elementos[index].curiosidade}</p>
+  `;
 }
+
+document.addEventListener('keydown', evt => {
+  const popUp = document.querySelector('.pop-up');
+  if (evt.key === 'Tab' || evt.code === 'Tab') {
+    if (evt.shiftKey) {
+      if (document.activeElement === popUp.firstElementChild) {
+        evt.preventDefault();
+        popUp.lastElementChild.focus();
+      }
+    } else {
+      if (document.activeElement === popUp.lastElementChild) {
+        evt.preventDefault();
+        popUp.firstElementChild.focus();
+      }
+    }
+  }
+});
