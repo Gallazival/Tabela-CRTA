48297d50d52adaab28908214bc651cc5af155f5c
pedromchd <pedrogmachado28@outlook.com>
2022-12-30 01:46:38 -0300

    rework da tabela em andamento

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
   }
 
   for (const row in tabela) {
-    $('[role=table]').append(`<div role="row" aria-rowindex="${parseInt(row) + 1}"></div>`);
+    $('table').append(`<tr role="row" aria-rowindex="${parseInt(row) + 1}"></tr>`);
     let aux = 2;
     for (const elemento of tabela[row]) {
       const classe = elemento.classe.replace(/\s/gm, '-');
@@ -21,23 +21,26 @@ $(document).ready(function () {
       if (elemento.classe === 'Lantanídeos' || elemento.classe === 'Actinídios') {
         index = ++aux;
       }
-      $('[role=row]').last().append(`
-        <button role="cell" aria-colindex="${index}" data-classe="${classe}">
-          ${elemento.numero}
-        </button>
+      $('tr').last().append(`
+        <td role="cell" aria-colindex="${index}" data-classe="${classe}" data-bs-toggle="modal" data-bs-target="#detalharElemento">
+          <span class="visually-hidden">${elemento.nome}</span>
+          <span class="numero"><span class="visually-hidden">Número atômico: </span>${elemento.numero}</span>
+          <span class="simbolo"><span class="visually-hidden">Símbolo: </span>${elemento.simbolo}</span>
+          <span class="massa"><span class="visually-hidden">Massa: </span>${elemento.massa}</span>
+        </td>
       `.trim());
     }
   }
 
   $('[aria-colindex="18"]').slice(0, 1)
-    .before('<div role="cell" aria-colspan="16" aria-hidden="true"></div>');
+    .before('<td role="cell" colspan="16" aria-colspan="16"></td>');
 
   $('[aria-colindex="13"]').slice(0, 2)
-    .before('<div role="cell" aria-colspan="10" aria-hidden="true"></div>');
+    .before('<td role="cell" colspan="10" aria-colspan="10"></td>');
 
   $('[aria-colindex="2"]').slice(-2)
-    .after('<div role="cell" aria-hidden="true"></div>');
+    .after('<td role="cell"></td>');
 
   $('[aria-colindex="3"]').slice(-2)
-    .before('<div role="cell" aria-colspan="2" aria-hidden="true"></div>');
+    .before('<td role="cell" colspan="2" aria-colspan="2"></td>');
 });
